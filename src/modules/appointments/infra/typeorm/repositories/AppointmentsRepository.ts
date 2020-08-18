ca43ab8544f678cd97c4c40d009b8762ac638081
Joao Vitor Fonseca da Silva <joaovfonseca@outlook.com>
2020-08-17 23:18:17 -0300

    API: Adding user to return with the appointments query

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -45,6 +45,7 @@ export default class AppointmentsRepository implements IAppointmentsRepository {
         date: Raw(dateFieldName => 
           `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`),
       },
+      relations: ['user']
     });
 
     return appointments;
